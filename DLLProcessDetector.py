61b7ed67ded1102a88c67679f41882c7583f8d8a
DarkGhost <95978207+DarkGhost010@users.noreply.github.com>
2022-10-02 10:29:48 +0300

    Add files via upload

---
+++ DLLProcessDetector.py
@@ -0,0 +1,50 @@
+dll_inject = []
+
+def process():
+    procs = session.plugins.pslist()
+    for eprocess in procs.filter_processes():
+        if(eprocess.name == "lsass.exe"):
+            continue
+        if(eprocess.name == "Notepad.exe"):
+            if(vad(eprocess)):
+                thread(eprocess.UniqueProcessId)
+
+
+def vad(sample):
+    a = True
+    b = False
+    vad_list = session.plugins.vad().collect_vadroot(sample.RealVadRoot, sample)
+    if(len(vad_list) < 0):
+        return b
+    for s in vad_list:
+        if(s['type'] == "Mapped" and s['protect'] == "EXECUTE_WRITECOPY" and s['filename'] != None):
+            return a
+
+
+def thread(pid):
+     t = session.plugins.threads(pid)
+     for k in t:
+       if("windows!DllCanUnloadNow+0x2f70" in str(k["win32_start_symb"])):
+                    dll_inject.append(pid)
+                    break
+     try:
+        if(len(dll_inject) > 0):
+            print("\t")
+            for pid in dll_inject:
+             print("Notepad has been infected  (PID: %d) " % (pid))
+        else:
+            print("Notepad has not been infected")
+     except Exception as p:
+        print(p)
+                 
+   
+
+        
+if __name__ == "__main__":
+    process()
+
+
+              
+  
+
+
